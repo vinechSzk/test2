f2090f717e183eaa0914b68f793e8bb0f343a51a
vinechSzk <vinech.szk@gmail.com>
2012-07-15 13:49:37 +0800

    Initial Commit

---
+++ test2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test2
+//
+//  Created by bin huang on 12-7-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
